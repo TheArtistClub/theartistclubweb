071ed583cd6ee5f0548625bdf021d12e9c31d5a2
Sergio Alexander <xergioalex@gmail.com>
2021-02-27 16:40:34 +0000

    Docker config added && Privacy policy updated

--- pages/legal/privacy-policy.vue
+++ pages/legal/privacy-policy.vue
@@ -1,21 +1,78 @@
 <template>
   <div class="container">
     <div class="content">
-      <a href="/">
-        <img class="logo" src="/images/logo.png" alt="The Artist Club Logo" />
-      </a>
-      <h2>Privacy Policy</h2>
-      <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
-      </p>
+      <div class="content-info">
+        <a href="/">
+          <img class="logo" src="/images/logo.png" alt="The Artist Club Logo" />
+        </a>
+
+        <h2>POLÍTICA DE PRIVACIDAD de el club del Artista</h2>
+
+        <p>Última modificación: 27 de febrero de 2021</p>
+        <p>El respeto a tu privacidad está totalmente garantizado.</p>
+
+        <ol>
+          <li>
+            La aplicación no guarda información relativa a tu dispositivo como,
+            por ejemplo, fallos, actividad del sistema, ajustes del hardware,
+            tipo de navegador, idioma del navegador.
+          </li>
+          <li>La aplicación no accede a tus contactos ni agendas.</li>
+          <li>
+            <strong>Clasificación por edades:</strong>
+            PEGI 3 - Apto para todos los públicos.
+          </li>
+          <li>
+            <strong>Cargos y cuotas:</strong>
+            Cualquier uso de esta aplicación es totalmente gratuito.
+          </li>
+          <li>
+            <strong>Cambios en nuestra Política de Privacidad:</strong>
+            Nuestra Política de Privacidad puede cambiar de vez en cuando.
+            Publicaremos cualquier cambio de política de privacidad en esta
+            página, por lo que debe revisarla periódicamente.
+          </li>
+          <li>
+            La aplicación utiliza el permiso de CAMERA, para permitir a los
+            artistas la publicación manual de sus obras.
+          </li>
+        </ol>
+
+        <br />
+        <br />
+
+        <h2>PRIVACY POLICY of the Artist's club</h2>
+
+        <p>Last modified: February 27, 2021</p>
+        <p>Respect for your privacy is fully guaranteed.</p>
+
+        <ol>
+          <li>
+            The application does not save information related to your device
+            such as failures, system activity, hardware settings, browser type,
+            browser language.
+          </li>
+          <li>The application does not access your contacts or agendas.</li>
+          <li>
+            <strong>Age rating:</strong>
+            PEGI 3 - Suitable for all audiences
+          </li>
+          <li>
+            <strong>Charges and fees:</strong>
+            Any use of this application is completely free.
+          </li>
+          <li>
+            <strong>Changes to our Privacy Policy:</strong>
+            Our Privacy Policy may change from time to time. We will post any
+            privacy policy changes on this page, so please check back
+            periodically.
+          </li>
+          <li>
+            The application uses the permission of CAMERA, to allow artists to
+            manually publish their works.
+          </li>
+        </ol>
+      </div>
     </div>
   </div>
 </template>
@@ -35,12 +92,22 @@ export default Vue.extend({})
 
 .content {
   padding: 0 20px;
-  .logo {
-    width: 200px;
-  }
-  h2 {
-    margin-top: 15px;
-    margin-bottom: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .content-info {
+    max-width: 720px;
+    .logo {
+      width: 200px;
+    }
+    h2 {
+      margin-top: 15px;
+      margin-bottom: 15px;
+    }
+    p {
+      margin-bottom: 10px;
+    }
   }
 }
 </style>
