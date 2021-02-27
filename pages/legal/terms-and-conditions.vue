db76a8143de4932dd60a4b8554718ef5dab38d72
Sergio Alexander <xergioalex@gmail.com>
2021-02-27 04:03:41 -0500

    Update copies

--- pages/legal/terms-and-conditions.vue
+++ pages/legal/terms-and-conditions.vue
@@ -14,7 +14,7 @@
         essentially unchanged. It was popularised in the 1960s with the release
         of Letraset sheets containing Lorem Ipsum passages, and more recently
         with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        of Lorem Ipsum..
       </p>
     </div>
   </div>
